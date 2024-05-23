014f4934833f6fdbeac6890d11d949a723d04a31
Bruno Tejeria <brunotejeria@hotmail.com>
2024-05-23 14:30:00 -0300

    🔧 Improve documentation.

--- source/schemas/data_validators.py
+++ source/schemas/data_validators.py
@@ -2,30 +2,58 @@ import re
 
 class Validators:
     """
-    A class to handle regular expressions.
+    A class to handle validation of various types of data using regular expressions.
     """
+
     @staticmethod
     def validate_email(email: str) -> bool:
         """
-        Validates an email address.
+        Validates an email address using a regular expression.
+
+        Parameters:
+        email (str): The email address to validate.
+
+        Returns:
+        bool: True if the email is valid, False otherwise.
         """
         return re.match(r"[^@]+@[^@]+\.[^@]+", email) is not None
 
     @staticmethod
     def validate_ssid(ssid: str) -> bool:
         """
-        Validates an SSID.
+        Validates an SSID to ensure it only contains alphanumeric characters and is not longer than 32 characters.
+
+        Parameters:
+        ssid (str): The SSID to validate.
+
+        Returns:
+        bool: True if the SSID is valid, False otherwise.
         """
         return re.match(r"^[a-zA-Z0-9]{1,32}$", ssid) is not None
 
     class file:
         @staticmethod
         def basic(string: str) -> bool:
+            """
+            Checks if a given string contains any file path separators.
+
+            Parameters:
+            string (str): The string to check.
+
+            Returns:
+            bool: True if the string contains path separators, False otherwise.
+            """
             return re.search(r"[./\\]", string)
 
+        @staticmethod
         def txt(string: str) -> bool:
+            """
+            Checks if a given string does not end with '.txt'.
+
+            Parameters:
+            string (str): The string to check.
+
+            Returns:
+            bool: True if the string does not end with '.txt', False otherwise.
+            """
             return string.endswith(".txt") is not True
-
-
-
-        
